e6a30736bb37dcec0720035ed8e001d05cdf2f2a
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-10-21 07:31:40 -0300

    remove debugging statements

--- main.rb
+++ main.rb
@@ -9,16 +9,13 @@ BASE_URL = "https://clipia.ca"
 bot = Discordrb::Bot.new(token: ENV['token'])
 
 bot.message do |e|
-    p e.message.to_s
     match = e.message.to_s.match(/https:\/\/(www\.)?clipia\.ca\/media\/(\d+)\/?/)
-    p match
     next unless match
     id = match[2]
     response = RestClient.get("#{BASE_URL}/api/v1/media/#{id}", {
         Authorization: "Bearer #{CLIPIA_KEY}"
     })
     json = JSON.parse(response.body)
-    p json
     embed = Discordrb::Webhooks::Embed.new(
         title: json['title'], 
         url: "#{BASE_URL}#{json['url']}", 
